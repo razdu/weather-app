274ddf491b44043602553ccdff8cad79501467e6
razdu <59321327+razdu@users.noreply.github.com>
2021-11-19 06:10:06 +0200

    remove extra lines

--- app.js
+++ app.js
@@ -41,19 +41,6 @@ $('#forecast').on('change', () => {
     );
 })
 
-/* weatherData = JSON.parse(localStorage.getItem('weatherData'));
-let deg = weatherData.wind.deg;
-let degDir = setDirection(deg);
-$('[data-city]').html(weatherData.name+", "+weatherData.sys.country);
-$('[data-feel]').html('feels like\n '+weatherData.main.feels_like + "&degc")
-$('[data-humidity]').html('Humidity\n '+weatherData.main.humidity + '%')
-$('[data-temp]').html('Temp\n '+weatherData.main.temp + "&degc")
-$('[data-maxTemp]').html('max\n '+weatherData.main.temp_max + "&degc")
-$('[data-minTemp]').html('min\n '+weatherData.main.temp_min + "&degc")
-$('[data-weather]').html('Info: '+weatherData.weather[0].description)
-$('[data-wind]').html(`Wind: ${degDir}(${deg}&deg)<br> at speed: 
-        ${weatherData.wind.speed} [<sup>Km</sup>/<sub>h</sub>]`)
- */
 function setDirection(deg){
     const dirCode = ["E","ENE","NNE","N","N","NNW","WNW","W","W","WWS","WSS","S","S","SSE","ESE","E"];
     let i = Math.floor(deg/22.5);
