65ba6f399968b4e4cfb71757eda49521557f5d30
Idan magled <idanmagled@gmail.com>
2021-11-20 14:19:16 +0200

    - Some more refactor.

--- app.js
+++ app.js
@@ -5,21 +5,20 @@ const appid = 'eb98981901058b162b2802e1ddd392b9';
 getInitData();
 
 $('#forecast').on('change', () => {
-    let selectedCity = $('.my-city-list option').filter(':selected').text();
-    $.getJSON('https://api.openweathermap.org/data/2.5/weather?', `q=${selectedCity}&appid=${appid}&units=metric`, function (mWeatherData, textStatus) {
+    $.getJSON('https://api.openweathermap.org/data/2.5/weather?', `q=${$('.my-city-list option').filter(':selected').text()}&appid=${appid}&units=metric`, function (mWeatherData, textStatus) {
         (textStatus !== 'success') ? alert('Can\'t get data') : populateWeatherData(mWeatherData)
     });
 })
 
 function populateWeatherData(weatherData) {
     // globalWeatherData = weatherData
-    $('[data-city]').html(weatherData.name + ", " + weatherData.sys.country);
-    $('[data-feel]').html('feels like\n ' + weatherData.main.feels_like + "&degc");
-    $('[data-humidity]').html('Humidity\n ' + weatherData.main.humidity + '%');
-    $('[data-temp]').html('Temp\n ' + weatherData.main.temp + "&degc");
-    $('[data-maxTemp]').html('max\n ' + weatherData.main.temp_max + "&degc");
-    $('[data-minTemp]').html('min\n ' + weatherData.main.temp_min + "&degc");
-    $('[data-weather]').html('Info: ' + weatherData.weather[0].description);
+    $('[data-city]').text(weatherData.name + ", " + weatherData.sys.country);
+    $('[data-feel]').text('feels like\n ' + weatherData.main.feels_like + "&degc");
+    $('[data-humidity]').text('Humidity\n ' + weatherData.main.humidity + '%');
+    $('[data-temp]').text('Temp\n ' + weatherData.main.temp + "&degc");
+    $('[data-maxTemp]').text('max\n ' + weatherData.main.temp_max + "&degc");
+    $('[data-minTemp]').text('min\n ' + weatherData.main.temp_min + "&degc");
+    $('[data-weather]').text('Info: ' + weatherData.weather[0].description);
     $('[data-wind]').html(`Wind: ${setDirection(weatherData.wind.deg)}(${weatherData.wind.deg}&weatherData.wind.deg)<br> at speed: 
                             ${weatherData.wind.speed} [<sup>Km</sup>/<sub>h</sub>]`);
 }
@@ -31,9 +30,9 @@ function setDirection(deg) {
 
 function getInitData() {
     $.getJSON("./il.cities.json", function (data) {
-        $("<select/>", {
+        $("#forecast").append($("<select/>", {
             "class": "my-city-list",
             html: data.map((item) => "<option value='" + item.city + "'>" + item.city + "</option>").join("")
-        }).appendTo("#forecast");
+        }));
     });
 }
